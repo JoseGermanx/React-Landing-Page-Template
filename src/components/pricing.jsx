e73b09059ec175c85f11c3ceb1f4f3ad2c0bc33d
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-11 00:01:57 -0400

    Ajuste con numero de telefono wa y css

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -3,50 +3,23 @@ import { PricingCard } from "./pricingCard";
 
 export const Pricing = (props) => {
   return (
-    <div className="container">
-      <div className="grid gap-5">
+    <div className="container my-sm-5 my-md-5 my-lg-5">
+      <div className="row">
         {props.data.length > 0
           ? props.data.map((e, id) => {
               return (
-                <div key={id} className="col-sm-6 col-md-4 col-lg-4">
+                <div key={id} className="col-sm-12 col-md-12 col-lg-4 my-sm-3 my-md-3 my-lg-5 my-3">
                   <PricingCard
-                    className="pricingCard"
                     title={e.title}
                     normal_price={e.normal_price}
                     promo_price={e.promo_price}
                     include={e.include}
                     channels={e.channels}
                     extra={e.extra}
+                    id={id}
+                    mas={e.mas}
+                    contacto={e.contacto}
                   />
-                  <div
-                    className="accordion accordion-flush"
-                    id="accordionFlushExample"
-                  >
-                    <div className="accordion-item">
-                      <h4 className="accordion-header">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target={`#flush-collapse${id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"}`}
-                          aria-expanded="false"
-                          aria-controls={`flush-collapse${id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"}`}
-                        >
-                          Mas detalles del plan
-                        </button>
-                      </h4>
-                      <div
-                        id={`flush-collapse${id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"}`}
-                        className="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample"
-                      >
-                        <div className="accordion-body">
-                          <p>{e.mas}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <a type="button" className="btn btn-success" href={`https://api.whatsapp.com/send?phone=${e.contacto}&text=Hola%2C%20quiero%20más%20información%20sobre%20el%20plan%20${e.title}`} target="blank">Contrata ahora</a>
                 </div>
               );
             })
--- src/components/pricingCard.jsx
+++ src/components/pricingCard.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./pricing.css";
 
-export const PricingCard = ({title, normal_price, promo_price, include, channels, extra }) => {
+export const PricingCard = ({
+  title,
+  normal_price,
+  promo_price,
+  include,
+  channels,
+  extra,
+  id,
+  mas,
+  contacto,
+}) => {
   return (
-    <div>
-      <div className="title"><h1>{title}</h1></div>
+    <div className="text-center d-flex flex-column align-content-between">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
       <div className="card">
         <h2 className="price">
           <span className="price__currency">$</span>
@@ -14,7 +26,48 @@ export const PricingCard = ({title, normal_price, promo_price, include, channels
         <p className="price-overview">{include}</p>
         <p className="description">{extra}</p>
       </div>
-     
+      <div className="accordion accordion-flush" id="accordionFlushExample">
+        <div className="accordion-item">
+          <h4 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target={`#flush-collapse${
+                id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"
+              }`}
+              aria-expanded="false"
+              aria-controls={`flush-collapse${
+                id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"
+              }`}
+            >
+              Mas detalles del plan
+            </button>
+          </h4>
+          <div
+            id={`flush-collapse${
+              id === 0 ? "One" : id === 1 ? "Two" : id === 2 && "Three"
+            }`}
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">
+              <p>{mas}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="btn btn-success w-50 align-self-center">
+      <i className="fa fa-whatsapp" aria-hidden="true"></i>
+      <a
+        className="text-white text-decoration-none px-2"
+        type="button"
+        href={`https://api.whatsapp.com/send?phone=${contacto}&text=Hola%2C%20quiero%20más%20información%20sobre%20el%20plan%20${title}`}
+        target="blank"
+      >
+        Contrata ahora
+      </a>
+      </div>
     </div>
   );
 };
