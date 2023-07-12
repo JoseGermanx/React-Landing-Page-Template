6b1822c707e4b6de16d31ea476b3a7740cfad124
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-12 09:08:29 -0400

    Ajuste de breackpoints en about

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -8,6 +8,11 @@ export const Navigation = (props) => {
       className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
     >
       <div className="container-fluid">
+        <div>
+          <a className="navbar-brand" href="#page-top">
+            RSA Telecomunicaciones
+          </a>{" "}
+        </div>
         <div className="navbar-header">
           <button
             class="navbar-toggler"
@@ -22,33 +27,32 @@ export const Navigation = (props) => {
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <div>
-          <a className="navbar-brand page-scroll" href="#page-top">
-              RSA Telecomunicaciones
-            </a>{" "}
-            </div>
-            <div>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#about">
-                  Acerca de RSA
-                </a>
-              </li>
-              <li class="nav-item">
-                <Link to="/contrata" className="btn btn-custom btn-lg">
-                  Contrata ahora
-                </Link>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#team">
-                  Trabaja con nosotros
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#contact">
-                  Contactos
-                </a>
-              </li>
-            </ul>
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#about">
+                    Acerca de RSA
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#team">
+                    Trabaja con nosotros
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    aria-current="page"
+                    href="#contact"
+                  >
+                    Contactos
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <Link to="/contrata" className="btn btn-custom btn-lg">
+                    Contrata ahora
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
